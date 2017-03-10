018d063ced788d48b7e1a57d18efa0236dcc2d7a
Katherine Yu <kyu@fanatics.com>
2017-03-09 21:59:17 -0800

    init distangle

---
+++ siamese-distangle/run.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Q2: Recurrent neural nets for NER
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+
+import argparse
+import logging
+import sys
+import time
+from datetime import datetime
+import copy
+
+import tensorflow as tf
+import numpy as np
+
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from util import print_sentence, write_conll, read_dat
+from data_util import load_and_preprocess_data, load_embeddings, ModelHelper
+from defs import LBLS
+from rnn_model import RNNModel, Config, logger
+
+
+def do_train(args):
+    # Set up some parameters.
+    config = Config(args)
+    helper, train_dat1, train_dat2, train_lab, dev_dat1, dev_dat2, dev_lab = load_and_preprocess_data(args)
+    train = zip(train_dat1, train_dat2, train_lab)
+    dev = zip(dev_dat1, dev_dat2, dev_lab)
+    embeddings = load_embeddings(args, helper)
+    config.embed_size = embeddings.shape[1]
+    helper.save(config.output_path)
+
+    handler = logging.FileHandler(config.log_output)
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
+    logging.getLogger().addHandler(handler)
+
+    report = None #Report(Config.eval_output)
+
+    with tf.Graph().as_default():
+        logger.info("Building model...",)
+        start = time.time()
+        model = RNNModel(helper, config, embeddings)
+        logger.info("took %.2f seconds", time.time() - start)
+
+        init = tf.global_variables_initializer()
+        saver = tf.train.Saver()
+
+        with tf.Session() as session:
+            session.run(init)
+            model.fit(session, saver, train, dev)
+            if report:
+                report.log_output(model.output(session, dev))
+                report.save()
+            else:
+                # Save predictions in a text file.
+                output = model.output(session, dev)
+                sentences, labels, predictions = zip(*output)
+                output = zip(sentences, labels, predictions)
+
+                with open(model.config.conll_output, 'w') as f:
+                    write_conll(f, output)
+                with open(model.config.eval_output, 'w') as f:
+                    for sentence, labels, predictions in output:
+                        print_sentence(f, sentence, labels, predictions)
+
+def do_evaluate(args):
+    config = Config(args.model_path)
+    helper = ModelHelper.load(args.model_path)
+    input_data = read_conll(args.data)
+    embeddings = load_embeddings(args, helper)
+    config.embed_size = embeddings.shape[1]
+
+    with tf.Graph().as_default():
+        logger.info("Building model...",)
+        start = time.time()
+        model = RNNModel(helper, config, embeddings)
+
+        logger.info("took %.2f seconds", time.time() - start)
+
+        init = tf.global_variables_initializer()
+        saver = tf.train.Saver()
+
+        with tf.Session() as session:
+            session.run(init)
+            saver.restore(session, model.config.model_output)
+            for sentence, labels, predictions in model.output(session, input_data):
+                predictions = [LBLS[l] for l in predictions]
+                print_sentence(args.output, sentence, labels, predictions)
+def do_test2(args):
+    logger.info("Testing implementation of RNNModel")
+    config = Config(args)
+    helper, train, dev, train_raw, dev_raw = load_and_preprocess_data(args)
+    embeddings = load_embeddings(args, helper)
+    config.embed_size = embeddings.shape[1]
+
+    with tf.Graph().as_default():
+        logger.info("Building model...",)
+        start = time.time()
+        model = RNNModel(helper, config, embeddings)
+        logger.info("took %.2f seconds", time.time() - start)
+
+        init = tf.global_variables_initializer()
+        saver = None
+
+        with tf.Session() as session:
+            session.run(init)
+            model.fit(session, saver, train, dev)
+
+    logger.info("Model did not crash!")
+    logger.info("Passed!")
+
+def do_shell(args):
+    config = Config(args.model_path)
+    helper = ModelHelper.load(args.model_path)
+    embeddings = load_embeddings(args, helper)
+    config.embed_size = embeddings.shape[1]
+
+    with tf.Graph().as_default():
+        logger.info("Building model...",)
+        start = time.time()
+        model = RNNModel(helper, config, embeddings)
+        logger.info("took %.2f seconds", time.time() - start)
+
+        init = tf.global_variables_initializer()
+        saver = tf.train.Saver()
+
+        with tf.Session() as session:
+            session.run(init)
+            saver.restore(session, model.config.model_output)
+
+            print("""Welcome!
+You can use this shell to explore the behavior of your model.
+Please enter sentences with spaces between tokens, e.g.,
+input> Germany 's representative to the European Union 's veterinary committee .
+""")
+            while True:
+                # Create simple REPL
+                try:
+                    sentence = raw_input("input> ")
+                    tokens = sentence.strip().split(" ")
+                    for sentence, _, predictions in model.output(session, [(tokens, ["O"] * len(tokens))]):
+                        predictions = [LBLS[l] for l in predictions]
+                        print_sentence(sys.stdout, sentence, [""] * len(tokens), predictions)
+                except EOFError:
+                    print("Closing session.")
+                    break
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Trains and tests an NER model')
+    subparsers = parser.add_subparsers()
+
+    command_parser = subparsers.add_parser('test2', help='')
+    command_parser.add_argument('-dt', '--data-train', type=argparse.FileType('r'), default="data/tiny.conll", help="Training data")
+    command_parser.add_argument('-dd', '--data-dev', type=argparse.FileType('r'), default="data/tiny.conll", help="Dev data")
+    command_parser.add_argument('-v', '--vocab', type=argparse.FileType('r'), default="data/vocab.txt", help="Path to vocabulary file")
+    command_parser.add_argument('-vv', '--vectors', type=argparse.FileType('r'), default="data/wordVectors.txt", help="Path to word vectors file")
+    command_parser.add_argument('-c', '--cell', choices=["rnn", "gru"], default="rnn", help="Type of RNN cell to use.")
+    command_parser.set_defaults(func=do_test2)
+
+    command_parser = subparsers.add_parser('train', help='')
+    command_parser.add_argument('-dt1', '--data-train1', dest='data_train1', type=argparse.FileType('r'))
+    command_parser.add_argument('-dt2', '--data-train2', dest='data_train2', type=argparse.FileType('r'))
+    command_parser.add_argument('-dtl', '--data-train-labels', dest='data_train_labels', type=argparse.FileType('r'))
+    command_parser.add_argument('-dd1', '--data-dev1', dest='data_dev1', type=argparse.FileType('r'))
+    command_parser.add_argument('-dd2', '--data-dev2', dest='data_dev2', type=argparse.FileType('r'))
+    command_parser.add_argument('-ddl', '--data-dev-labels', dest='data_dev_labels', type=argparse.FileType('r'))
+    command_parser.add_argument('-v', '--vocab', type=argparse.FileType('r'), default="data/glvocab_1_100.txt", help="Path to vocabulary file")
+    command_parser.add_argument('-vv', '--vectors', type=argparse.FileType('r'), default="data/glwordvectors_1_100.txt", help="Path to word vectors file")
+    command_parser.add_argument('-c', '--cell', choices=["rnn", "gru", "lstm"], default="lstm", help="Type of RNN cell to use.")
+    command_parser.add_argument('-eb', '--embed_size', dest='embed_size', default=100)
+    command_parser.set_defaults(func=do_train)
+
+    command_parser = subparsers.add_parser('evaluate', help='')
+    command_parser.add_argument('-d', '--data', type=argparse.FileType('r'), default="data/dev.conll", help="Training data")
+    command_parser.add_argument('-m', '--model-path', help="Training data")
+    command_parser.add_argument('-v', '--vocab', type=argparse.FileType('r'), default="data/vocab.txt", help="Path to vocabulary file")
+    command_parser.add_argument('-vv', '--vectors', type=argparse.FileType('r'), default="data/wordVectors.txt", help="Path to word vectors file")
+    command_parser.add_argument('-c', '--cell', choices=["rnn", "gru"], default="rnn", help="Type of RNN cell to use.")
+    command_parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=sys.stdout, help="Training data")
+    command_parser.set_defaults(func=do_evaluate)
+
+    command_parser = subparsers.add_parser('shell', help='')
+    command_parser.add_argument('-m', '--model-path', help="Training data")
+    command_parser.add_argument('-v', '--vocab', type=argparse.FileType('r'), default="data/vocab.txt", help="Path to vocabulary file")
+    command_parser.add_argument('-vv', '--vectors', type=argparse.FileType('r'), default="data/wordVectors.txt", help="Path to word vectors file")
+    command_parser.add_argument('-c', '--cell', choices=["rnn", "gru"], default="rnn", help="Type of RNN cell to use.")
+    command_parser.set_defaults(func=do_shell)
+
+    ARGS = parser.parse_args()
+    if ARGS.func is None:
+        parser.print_help()
+        sys.exit(1)
+    else:
+        ARGS.func(ARGS)
